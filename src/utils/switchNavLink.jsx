2bd7e4fd0b832bd1b6d071b2428b09bef2eb5d3a
dileine <mar.audiovisuals@gmail.com>
2023-07-11 12:06:00 +0200

    improvements

--- src/utils/switchNavLink.jsx
+++ src/utils/switchNavLink.jsx
@@ -1,11 +1,11 @@
-export const SwitchNavLink = (state, action) => {
-    switch (action.type) {
+ export const SwitchNavLink = (state, {type}) => {
+    switch (type) {
       case "homeClick":
         return { home: true, starships: false };
       case "starshipsClick":
         return { home: false, starships: true };
       case "none":
-        return {home: false, starships: false,}
+        return {home: false, starships: false,};
       default:
         return state;
     }
